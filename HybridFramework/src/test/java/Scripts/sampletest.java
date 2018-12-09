743f226baf485ac315d702695498b8995013abc9
Sai <Sai@ae089952.ibm.com>
2018-12-09 16:31:31 +0400

    Test for Sampletest file

--- HybridFramework/src/test/java/Scripts/sampletest.java
+++ HybridFramework/src/test/java/Scripts/sampletest.java
@@ -21,7 +21,7 @@ public class sampletest extends StartBrowser {
 			
 			StartBrowser.ChildTest = StartBrowser.ParentTest.createNode("Launching Web Check-in portal");
 			aDriver.launchapplication("https://ppe-wci.etihad.com/app#/check-in/search");
-			aDriver.type(HomePage.pnrdetails, "KAZRBI", "PNR ");
+			aDriver.type(HomePage.pnrdetails, "GGOKDC", "PNR ");
 			aDriver.type(HomePage.lastname, "WCI", "last name");
 			aDriver.waitForLoad(StartBrowser.driver, "Wait Function");
 			aDriver.Click(HomePage.btnsearchflight, "Search Flight Button");
